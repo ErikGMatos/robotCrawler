da5bb812ce8dae6a006fdf528cc463cbe70bd3cb
Erik Garces Matos <erik_senac@hotmail.com>
2018-06-20 14:14:19 -0300

    Update qb2.js

--- qb2.js
+++ qb2.js
@@ -190,7 +190,7 @@ async function SQLdados(){
 async function funcaoGravarDados(data){
     var sql = require('mssql');
     try {
-        const pool = await sql.connect('mssql://sa:homologacao@201.45.136.166:49700/CaptacaoConversaoGraduacao');
+        const pool = await sql.connect('mssql://sa:homologacao@201.000.000.000:40000/bancodedados');
         for (let i = 0; i < data.length; i++){  
             var dados = data[i];
             await sql.query`INSERT INTO dbo.PesquisaDeMercado (Origem,Faculdade,Curso,Modalidade,Turno,Local,PrecoSemDesconto,Bolsa,Preco,DataPesquisa,CidadePesquisa)
